385e12cc46f4ffa4b9a78df9a7aeadfb5cd2ece4
GeraldAC <200332@unsaac.edu.pe>
2025-06-11 08:17:18 -0500

    Initial commit with Vite + React + Chakra UI v2

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Box, Button, Heading } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Box p={6}>
+      <Heading mb={4}>Hola desde Chakra UI v2</Heading>
+      <Button colorScheme="teal">Botón Chakra</Button>
+    </Box>
+  );
+}
+
+export default App;
+
